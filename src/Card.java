c1bfe0a5b03e91b81ab5b8513819ac1b33d9415c
eminkartci <emin.kartci@ozu.edu.tr>
2022-04-13 00:16:13 +0300

    Initial Commit - Card Class Started

---
+++ src/Card.java
@@ -0,0 +1,59 @@
+
+public class Card {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Card c = new Card(1,"Fortune");
+		c.showCard();
+
+	}
+	
+	// CONSTANT
+	
+	// Attribute
+	int card_id;
+	String title,cardStr;
+	double value;
+	
+	// Constructor
+	public Card(int card_id,String title) {
+		
+		this.card_id 	= card_id;
+		this.title 		= title;
+		this.value		= 0;
+		this.cardStr 	= getCardStr();
+	}
+	
+	// Behaviour
+	public void showCard() {
+		
+		System.out.println(this.cardStr);
+		
+	}
+	
+	public String getCardStr() {
+		String content =  		"- "+this.title+"\t- " + "\n" +
+					"|                |" + "\n" +
+					"|                |" + "\n" +
+					"|                |" + "\n" +
+					"|                |" + "\n" +
+					"|                |" + "\n" +
+					"|                |" + "\n" +
+					"|                |" + "\n" +
+					"|                |" + "\n" +
+					"|                |" + "\n" +
+					"------------------ " + "\n" ;
+		
+		content += getCardInfo();
+		
+		return content;
+	}
+	
+	public String getCardInfo() {
+		return "Value " + this.value;
+	}
+	
+	
+	// GETTER SETTER
+
+}
